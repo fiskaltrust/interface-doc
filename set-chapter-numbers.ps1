f3b51839a51578277dfd0524b6adaa420406818a
Tom Schmiedlechner <tom.schmiedlechner@fiskaltrust.services>
2019-05-22 09:21:06 +0200

    Fixed chapter number script

--- set-chapter-numbers.ps1
+++ set-chapter-numbers.ps1
@@ -1,7 +1,7 @@
 # Get chapter numbering tool from GitHub
 New-Item -Name "tools" -ItemType Directory -Force
 Invoke-WebRequest -Uri https://github.com/TSchmiedlechner/DocFxChapterNumbers/releases/download/v1.0.2/DocFxChapterNumbers-v1.0.2.zip -OutFile "./tools/DocFxChapterNumbers.zip"
-Expand-Archive "./tools/DocFxChapterNumbers.zip" -DestinationPath "./tools/DocFxChapterNumbers"
+Expand-Archive "./tools/DocFxChapterNumbers.zip" -DestinationPath "./tools/DocFxChapterNumbers" -Force
 
-Move-Item doc doc_prev
-.\tools\DocFxChapterNumbers\DocFxChapterNumbers.exe doc_prev doc --force
+Rename-Item ./doc ./doc_prev
+./tools/DocFxChapterNumbers/DocFxChapterNumbers.exe ./doc_prev ./doc --force

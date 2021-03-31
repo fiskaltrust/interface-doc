b9c400ffa74128d047d4b73a97c8d02eb2ec8fbc
Andreas Edler <64966100+aedler-ft@users.noreply.github.com>
2021-03-31 08:43:08 +0200

    moved integration-overview to the submenue

--- toc.js
+++ toc.js
@@ -44,11 +44,11 @@ module.exports = {
           items: [
             'middleware-doc/doc/appendix-de-kassensichv/appendix-de-kassensichv',
             'middleware-doc/doc/appendix-de-kassensichv/terminology/terminology',
-            'middleware-doc/doc/appendix-de-kassensichv/cash-register-integration/cash-register-integration',
 			 {
               type: 'category',
               label: 'Cash register integration',
               items: [
+				'middleware-doc/doc/appendix-de-kassensichv/cash-register-integration/cash-register-integration',
                 'middleware-doc/doc/appendix-de-kassensichv/cash-register-integration/single-receipt-creation',
                 'middleware-doc/doc/appendix-de-kassensichv/cash-register-integration/receipt-sequences-creation',
               ]

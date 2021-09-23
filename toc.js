d1d0c2fa504c9ca72934223ee3613760d954571f
Mijo Milicevic <mijo.milicevic@fiskaltrust.at>
2021-09-23 10:46:21 +0200

    toc file formatting

--- toc.js
+++ toc.js
@@ -16,10 +16,10 @@ module.exports = {
               type: 'category',
               label: 'Cash register integration',
               items: [
-				        'middleware-doc/doc/general/cash-register-integration/cash-register-integration-regular-workflow',
-                'middleware-doc/doc/general/cash-register-integration/cash-register-integration-failure-scenarios',
+                 'middleware-doc/doc/general/cash-register-integration/cash-register-integration-regular-workflow',
+                 'middleware-doc/doc/general/cash-register-integration/cash-register-integration-failure-scenarios'
               ]
-            },
+           },
             'middleware-doc/doc/general/data-structures/data-structures',
             'middleware-doc/doc/general/function-structures/function-structures',
             'middleware-doc/doc/general/communication/communication',
